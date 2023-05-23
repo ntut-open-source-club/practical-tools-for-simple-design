d58c1b133544d4e15cfe7f03116930874ec52e94
noobdy <htommyf12@gmail.com>
2023-05-23 11:25:43 +0800

    move SDL_image.h and SDL_ttf.h to pch

--- include/pch.hpp
+++ include/pch.hpp
@@ -7,9 +7,12 @@
 
 #include <SDL.h>
 #include <SDL_events.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #include <GL/glew.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtx/matrix_transform_2d.hpp>
 
 #endif
--- src/Core/Context.cpp
+++ src/Core/Context.cpp
@@ -1,8 +1,6 @@
 #include "Core/Context.hpp"
 
-#include <SDL_image.h>
 #include <SDL_opengl.h>
-#include <SDL_ttf.h>
 #include <SDL_video.h>
 
 #include "Core/DebugMessageCallback.hpp"
