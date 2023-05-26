35436d7a18b9e808a894ad37d614e61b3c7c9371
DF <dragonflybaby2.0@gmail.com>
2023-05-26 18:55:25 +0800

    Use ChatGPT to generate document of Util::BGM

--- include/Util/BGM.hpp
+++ include/Util/BGM.hpp
@@ -4,20 +4,79 @@
 #include <memory>
 #include <string>
 namespace Util {
-
+/**
+ * @class BGM
+ * @brief Wrapper class for handling background music.
+ */
 class BGM {
 public:
+    /**
+     * @brief Default constructor for creating a BGM object.
+     * @note This constructor does not load any media.
+     *            The LoadMedia() function <b>MUST</b> be called afterward.
+     */
     BGM() = default;
+
+    /**
+     * @brief Constructor that initializes the BGM object and loads the music from the specified file path.
+     * @param path The file path of the background music to be loaded.
+     */
     BGM(const std::string &path);
 
+    /**
+     * @brief Deleted copy assignment operator to prevent copying of BGM objects.
+     */
+    BGM &operator=(const BGM &) = delete;
+
+    /**
+     * @brief Deleted copy constructor to prevent copying of BGM objects.
+     */
+    BGM(const BGM &) = delete;
+
+    /**
+     * @brief Retrieves the current volume of the background music.
+     * @return The current volume of the background music.
+     */
     int GetVolume() const;
 
+    /**
+     * @brief Sets the volume of the background music.
+     * @param volume The desired volume level for the background music.
+     */
     void SetVolume(const int &volume);
 
+    /**
+     * @brief Loads the background music from the specified file path.
+     * @param path The file path of the background music to be loaded.
+     */
     void LoadMedia(const std::string &path);
+
+    /**
+     * @brief Increases the volume of the background music by one.
+     */
     void VolumeUp();
+
+    /**
+     * @brief Decreases the volume of the background music by one.
+     */
     void VolumeDown();
+
+    /**
+     * @brief Plays the background music.
+     * @param loop The number of times the music will loop.<br>
+     *             A value of -1 means it will loop indefinitely.<br>
+     *             A non-negative value means it will loop that many times.<br>
+     *             Default value : -1
+     * @note Calling this method replaces the currently playing BGM.
+     */
     void Play(const int &loop = -1);
+
+    /**
+     * @brief Fades in the background music gradually.
+     * @param tick The duration of the fade-in effect, in milliseconds.
+     * @param loop The number of times the music will loop after the fade-in is complete.
+     *             A value of -1 means it will loop indefinitely.
+     */
     void FadeIn(const int &tick, const int &loop = -1);
 
 private:
@@ -25,6 +84,7 @@ private:
         std::unique_ptr<Mix_Music, void (*)(Mix_Music *)>(nullptr,
                                                           Mix_FreeMusic);
 };
+
 
 } // namespace Util
 
