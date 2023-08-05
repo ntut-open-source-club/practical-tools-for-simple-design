d9331ce0ef19eb5664ca46320357e7309ceb8e74
Rick A <t110590016@ntut.org.tw>
2023-08-05 14:50:07 +0800

    🎨 format

--- src/Util/Transform.cpp
+++ src/Util/Transform.cpp
@@ -1,30 +1,30 @@
 #include "Util/Transform.hpp"
 
 namespace Util {
-Transform Transform::Rotate(float radians) const {
+Transform Transform::Rotate(float r) const { // NOLINT
     glm::mat3 rotationMatrix = {
-        std::cos(radians) , std::sin(radians), 0.0F,
-        -std::sin(radians), std::cos(radians), 0.0F,
-        0.0F              ,  0.0F            , 1.0F,
-    }; // dont forget transpose your view
-    return Transform(rotationMatrix * m_Mat3);
+        std::cos(r),  std::sin(r), 0.0F, //
+        -std::sin(r), std::cos(r), 0.0F, //
+        0.0F,         0.0F,        1.0F, //
+    };                                   // dont forget transpose your view
+    return {rotationMatrix * m_Mat3};
 }
 
-Transform Transform::Translate(const glm::vec2& t) const {   
+Transform Transform::Translate(const glm::vec2 &t) const { // NOLINT
     glm::mat3 translationMatrix = {
-        1.0F, 0.0F, 0.0F,
-        0.0F, 1.0F, 0.0F,
-        t.x , t.y , 1.0F,
-    }; // dont forget transpose your view
-    return Transform(translationMatrix * m_Mat3);
+        1.0F, 0.0F, 0.0F, //
+        0.0F, 1.0F, 0.0F, //
+        t.x,  t.y,  1.0F, //
+    };                    // dont forget transpose your view
+    return {translationMatrix * m_Mat3};
 }
 
-Transform Transform::Scale(const glm::vec2& s) const {
+Transform Transform::Scale(const glm::vec2 &s) const { // NOLINT
     glm::mat3 scaleMatrix = {
-        s.x , 0.0F, 0.0F,
-        0.0F, s.y , 0.0F,
-        0.0F, 0.0F, 1.0F,
+        s.x,  0.0F, 0.0F, //
+        0.0F, s.y,  0.0F, //
+        0.0F, 0.0F, 1.0F, //
     };
-    return Transform(scaleMatrix * m_Mat3);
+    return {scaleMatrix * m_Mat3};
 }
 } // namespace Util
