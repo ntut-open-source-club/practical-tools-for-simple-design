a9ae14e69abfcd508aa3a3d6405104903ba16787
Rick <t110590016@ntut.org.tw>
2023-08-27 18:06:28 +0800

    🚚 change the order to TRS

--- include/Util/Transform.hpp
+++ include/Util/Transform.hpp
@@ -9,16 +9,16 @@ class Transform {
 public:
     Transform() : m_Mat3(glm::mat3(1.0F)) {};
 
-    Transform Rotate(float radians) const;
     Transform Translate(const glm::vec2& translation) const;
+    Transform Rotate(float radians) const;
     Transform Scale(const glm::vec2& scale) const;
 
-    void SetRotation(float radians);
     void SetTranslation(const glm::vec2& translation);
+    void SetRotation(float radians);
     void SetScale(const glm::vec2& scale);
 
-    float GetRotation() const;
     glm::vec2 GetTranslation() const;
+    float GetRotation() const;
     glm::vec2 GetScale() const;
 
     Transform operator*(const Transform& other) { return {m_Mat3 * other.m_Mat3}; }
