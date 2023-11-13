85f50ef62141f3a8ede8a36db0946cf9e8cf408d
df <dragonflybaby2.0@gmail.com>
2023-11-13 14:15:01 +0800

    Enable OpenGL depth testing in Context
    Enabled depth testing in the OpenGL Context initialization to fix occlusion issues in the 3D space. Earlier, the absence of depth testing led to erroneous renderings when objects overlapped. With depth testing enabled, objects are rendered correctly based on their depth in the 3D space, resolving the occlusion issues.

--- src/Core/Context.cpp
+++ src/Core/Context.cpp
@@ -72,6 +72,7 @@ Context::Context() {
     glDebugMessageCallback(Core::OpenGLDebugMessageCallback, nullptr);
 #endif
 
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
