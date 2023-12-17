da885ee9e473c968d23ab25ab5d8d9f09a3c14f5
DF <dragonflybaby2.0@gmail.com>
2023-12-17 10:56:35 +0800

    reverse y axis

--- src/Util/Input.cpp
+++ src/Util/Input.cpp
@@ -55,7 +55,7 @@ void Input::Update() {
     s_CursorPosition.y = static_cast<float>(y);
 
     s_CursorPosition.x -= WINDOW_WIDTH / 2;
-    s_CursorPosition.y -= WINDOW_HEIGHT / 2;
+    s_CursorPosition.y = -(s_CursorPosition.y - WINDOW_HEIGHT / 2);
 
     s_LBPressed = s_RBPressed = s_MBPressed = s_Scroll = s_MouseMoving = false;
 
