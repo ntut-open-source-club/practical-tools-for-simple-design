935759da60d932a83c045b20bcadd85d4334983d
df <dragonflybaby2.0@gmail.com>
2024-01-29 16:44:18 +0800

    Doc: Document class Logger and its methods
    - Added documentation for the Logger namespace and its associated marcos

--- include/Util/Logger.hpp
+++ include/Util/Logger.hpp
@@ -6,6 +6,13 @@
 #include <spdlog/spdlog.h>
 
 namespace Util::Logger {
+
+/**
+ * @enum Level
+ * @brief Enum representing the logging levels.
+ *
+ * This enum encapsulates the different levels of logging.
+ */
 enum class Level {
     TRACE,
     DEBUG,
@@ -15,9 +22,29 @@ enum class Level {
     CRITICAL,
 };
 
+/**
+ * @brief Initializes the logger.
+ *
+ * This function initializes the logger for the application.
+ */
 void Init();
 
+/**
+ * @brief Sets the logging level.
+ *
+ * This function sets the logging level for the application.
+ *
+ * @param level The new logging level.
+ */
 void SetLevel(Level level);
+
+/**
+ * @brief Gets the current logging level.
+ *
+ * This function returns the current logging level of the application.
+ *
+ * @return The current logging level.
+ */
 Level GetLevel();
 
 #define LOG_TRACE(...) spdlog::trace(__VA_ARGS__)
