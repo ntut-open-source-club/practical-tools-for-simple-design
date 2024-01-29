9e9085c2be26dad1664d2e4e47a4bb75ea33e692
df <dragonflybaby2.0@gmail.com>
2024-01-29 16:41:24 +0800

    Doc: Document class Image and its methods
    - Added documentation for the Image class and its associated methods

--- include/Util/Image.hpp
+++ include/Util/Image.hpp
@@ -12,17 +12,52 @@
 #include "Util/Transform.hpp"
 
 namespace Util {
+
+/**
+ * @class Image
+ * @brief A class representing an image.
+ *
+ * This class encapsulates the properties and behaviors of an image.
+ * It includes properties such as texture and surface.
+ * It also includes behaviors such as drawing the image.
+ */
 class Image : public Core::Drawable {
 public:
-    Image(const std::string &filepath);
+    /**
+     * @brief Constructor that takes a file path to the image.
+     *
+     * @param filepath The file path to the image.
+     */
+    explicit Image(const std::string &filepath);
 
+    /**
+     * @brief Draws the image with a given transform and z-index.
+     *
+     * This function draws the image at the specified z-index and applies the given transform.
+     *
+     * @param transform The transform to apply to the image.
+     * @param zIndex The z-index at which to draw the image.
+     */
     void Draw(const Util::Transform &transform, const float zIndex) override;
 
 private:
+    /**
+     * @brief Initializes the shader program.
+     */
     void InitProgram();
+
+    /**
+     * @brief Initializes the vertex array.
+     */
     void InitVertexArray();
-    void
-    InitUniformBuffer(const Util::Transform &transform = Util::Transform(), const float zIndex = -1);
+
+    /**
+     * @brief Initializes the uniform buffer.
+     *
+     * @param transform The transform to apply to the image.
+     * @param zIndex The z-index at which to draw the image.
+     */
+    void InitUniformBuffer(const Util::Transform &transform = Util::Transform(), const float zIndex = -1);
 
     static constexpr int UNIFORM_SURFACE_LOCATION = 0;
 
