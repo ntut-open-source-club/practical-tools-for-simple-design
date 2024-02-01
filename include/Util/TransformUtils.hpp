df3dec2e73264c9a689b5c408bad73c00d593240
DF <dragonflybaby2.0@gmail.com>
2024-02-01 21:18:33 +0800

    Doc: Document TransformUtils functions
    — Added documentation for the Transform utility functions

--- include/Util/TransformUtils.hpp
+++ include/Util/TransformUtils.hpp
@@ -8,8 +8,28 @@
 
 namespace Util {
 
+/**
+ * @brief Converts a Transform object into a 4x4 matrix.
+ *
+ * This function takes a Transform object and a float representing the z-index
+ * as input. It returns a 4x4 matrix representing the transformation.
+ *
+ * @param transform The Transform object to be converted.
+ * @param zIndex The z-index of the transformation.
+ * @return A 4x4 matrix representing the transformation.
+ */
 glm::mat4 TransformToMat4(const Transform &transform, float zIndex);
 
+/**
+ * @brief Converts a Transform object into uniform buffer data.
+ *
+ * Converts transform data in Core::UniformBuffer format.
+ * Call it and pass the returned value to Core::UniformBuffer->setData().
+ *
+ * @param transform The Transform object to be converted.
+ * @param zIndex The z-index of the transformation.
+ * @return A Matrices object representing the uniform buffer data.
+ */
 Core::Matrices ConvertToUniformBufferData(const Util::Transform &transform,
                                           float zIndex);
 
