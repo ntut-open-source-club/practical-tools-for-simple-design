3ae8e1cc67e1bf023e935ebba2d0b80c7534c2af
ntut-xuan <t109590031@ntut.org.tw>
2024-02-26 15:25:22 +0800

    Chore: Move the implementation to cpp

--- include/Util/MissingTexture.hpp
+++ include/Util/MissingTexture.hpp
@@ -13,15 +13,6 @@
 static constexpr auto MISSING_TEXTURE_BASE64_DECODE_LENGTH = DecodeBase64Length(MISSING_TEXTURE);
 static constexpr auto MISSING_TEXTURE_BASE64_DECODE = DecodeBase64<MISSING_TEXTURE_BASE64_DECODE_LENGTH>(MISSING_TEXTURE);
 
-SDL_Surface* GetMissingTextureSDLSurface(){
-    SDL_RWops* rwop = SDL_RWFromConstMem(MISSING_TEXTURE_BASE64_DECODE.data(), MISSING_TEXTURE_BASE64_DECODE.size());
-    SDL_Surface* aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
-
-    if (aSurface == nullptr) {
-        LOG_ERROR("base64ToSurface");
-    }
-
-    return aSurface;
-}
+SDL_Surface* GetMissingTextureSDLSurface();
 
 #endif
---
+++ src/Util/MissingTexture.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 黃漢軒 on 2024/2/26.
+//
+#include <SDL.h>
+#include <SDL_image.h>
+#include <Util/Logger.hpp>
+
+#include "Util/MissingTexture.hpp"
+
+SDL_Surface* GetMissingTextureSDLSurface(){
+    SDL_RWops* rwop = SDL_RWFromConstMem(MISSING_TEXTURE_BASE64_DECODE.data(), MISSING_TEXTURE_BASE64_DECODE.size());
+    SDL_Surface* aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
+
+    if (aSurface == nullptr) {
+        LOG_ERROR("base64ToSurface");
+    }
+
+    return aSurface;
+}
