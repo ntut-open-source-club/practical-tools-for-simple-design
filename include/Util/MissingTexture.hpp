eb30b506ab9b903bc8bb1a3e79b9f184c2da6adb
ntut-xuan <t109590031@ntut.org.tw>
2024-02-29 00:58:29 +0800

    Chore: Format

--- include/Util/MissingTexture.hpp
+++ include/Util/MissingTexture.hpp
@@ -1,18 +1,20 @@
 #ifndef UTIL_MISSING_TEXTURE_HPP
 #define UTIL_MISSING_TEXTURE_HPP
 
-#include <string>
 #include <SDL.h>
 #include <SDL_image.h>
-#include <iostream>
 #include <Util/Logger.hpp>
+#include <iostream>
+#include <string>
 
 #include "Base64.hpp"
 #include "Core/MissingTextureBase64.hpp"
 
-static constexpr auto MISSING_TEXTURE_BASE64_DECODE_LENGTH = DecodeBase64Length(MISSING_TEXTURE);
-static constexpr auto MISSING_TEXTURE_BASE64_DECODE = DecodeBase64<MISSING_TEXTURE_BASE64_DECODE_LENGTH>(MISSING_TEXTURE);
+static constexpr auto MISSING_TEXTURE_BASE64_DECODE_LENGTH =
+    DecodeBase64Length(MISSING_TEXTURE);
+static constexpr auto MISSING_TEXTURE_BASE64_DECODE =
+    DecodeBase64<MISSING_TEXTURE_BASE64_DECODE_LENGTH>(MISSING_TEXTURE);
 
-SDL_Surface* GetMissingTextureSDLSurface();
+SDL_Surface *GetMissingTextureSDLSurface();
 
 #endif
