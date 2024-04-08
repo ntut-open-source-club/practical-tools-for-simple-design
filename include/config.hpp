0c3d1c3127cbf538551b8c883100d46138abb380
ZRE <chy853@gmail.com>
2024-04-09 00:41:23 +0800

    Update FPS_CAP value to 60 in config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -20,6 +20,6 @@ constexpr Util::Logger::Level DEFAULT_LOG_LEVEL = Util::Logger::Level::DEBUG;
  *
  * Set value to 0 to turn off FPS cap
  */
-constexpr unsigned int FPS_CAP = 0;
+constexpr unsigned int FPS_CAP = 60;
 
 #endif
