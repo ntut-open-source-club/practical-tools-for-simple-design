5ada101603037809549ce7dfa0bdc01fa0fb6ce7
Rick <t110590016@ntut.edu.tw>
2024-04-12 04:05:46 +0800

    Revert "Add logging support"
    This reverts commit 2274ffbccfddf80d8b3465c2bb9236677d1241e1.

--- include/Util/Logger.hpp
+++ include/Util/Logger.hpp
@@ -89,22 +89,6 @@ struct fmt::formatter<Util::Transform> : fmt::formatter<std::string> {
                          t.rotation, t.scale);
     }
 };
-
-template <>
-struct fmt::formatter<Util::PTSDPosition> : fmt::formatter<std::string> {
-    auto format(const Util::PTSDPosition &p, format_context &ctx) const
-        -> decltype(ctx.out()) {
-        return format_to(ctx.out(), "PTSDPos ({}, {})", p.x, p.y);
-    }
-};
-
-template <>
-struct fmt::formatter<Util::SDLPosition> : fmt::formatter<std::string> {
-    auto format(const Util::SDLPosition &p, format_context &ctx) const
-        -> decltype(ctx.out()) {
-        return format_to(ctx.out(), "SDLPos ({}, {})", p.x, p.y);
-    }
-};
 // NOLINTEND
 
 #endif
