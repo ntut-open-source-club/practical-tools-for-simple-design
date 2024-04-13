fe65951dbb9ab298da94e6ac27a7940580fc670f
Rick <t110590016@ntut.edu.tw>
2024-04-13 23:01:27 +0800

    useful conversions
    I guess la

--- include/Util/Position.hpp
+++ include/Util/Position.hpp
@@ -14,12 +14,19 @@ struct SDLPosition {
     SDLPosition(int x, int y) : x{x}, y{y} {};
     explicit SDLPosition(PTSDPosition&&);
     explicit SDLPosition(const PTSDPosition&);
+    explicit SDLPosition(const glm::vec2& v) :
+        x(static_cast<int>(v.x)),
+        y(static_cast<int>(v.y)) {}
+
+    explicit operator glm::vec2() const {
+        return {x, y};
+    }
 
     SDLPosition operator+(const glm::vec2&&) const;
     SDLPosition operator-(const glm::vec2&& vec2) const {
         return (*this) + (vec2*(-1.0f));
     }
-
+    
     PTSDPosition toPTSDPosition() const;
 };
 
@@ -30,10 +37,17 @@ struct PTSDPosition {
     PTSDPosition(SDLPosition&&);
     PTSDPosition(const SDLPosition&);
     PTSDPosition(float x, float y) : x{x}, y{y} {};
+    [[deprecated("Implicit conversion will be removed. Use explicit conversion instead")]]
+    PTSDPosition(glm::vec2 v) : x{v.x}, y{v.y} {};
 
     PTSDPosition operator+(const glm::vec2) const;
     PTSDPosition operator-(const glm::vec2 vec2) const {
         return (*this) + (vec2*(-1.0f));
+    }
+    
+    [[deprecated("Implicit conversion will be removed. Use explicit conversion instead")]]
+    operator glm::vec2() const {
+        return {x, y};
     }
     
     SDLPosition toSDLPosition() const;
