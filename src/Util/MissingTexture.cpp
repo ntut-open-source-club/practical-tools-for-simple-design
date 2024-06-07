ec03fa64254b6ea00bfc64ab7bd6dd2dd892bd04
ntut-xuan <t109590031@ntut.org.tw>
2024-06-07 19:44:03 +0800

    Fix: Should in Util namespace

--- src/Util/MissingTexture.cpp
+++ src/Util/MissingTexture.cpp
@@ -4,28 +4,30 @@
 
 #include "Util/MissingTexture.hpp"
 
-SDL_Surface *GetMissingImageTextureSDLSurface() {
-    SDL_RWops *rwop =
-        SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
-                           MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
-    SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
+namespace Util {
+    SDL_Surface *GetMissingImageTextureSDLSurface() {
+        SDL_RWops *rwop =
+                SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
+                                   MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
+        SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
 
-    if (aSurface == nullptr) {
-        LOG_ERROR("base64ToSurface");
+        if (aSurface == nullptr) {
+            LOG_ERROR("base64ToSurface");
+        }
+
+        return aSurface;
     }
 
-    return aSurface;
-}
+    SDL_Surface *GetMissingFontTextureSDLSurface() {
+        SDL_RWops *rwop =
+                SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
+                                   MISSING_FONT_TEXTURE_BASE64_DECODE.size());
+        SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "JPG");
 
-SDL_Surface *GetMissingFontTextureSDLSurface() {
-    SDL_RWops *rwop =
-        SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
-                           MISSING_FONT_TEXTURE_BASE64_DECODE.size());
-    SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "JPG");
+        if (aSurface == nullptr) {
+            LOG_ERROR("base64ToSurface");
+        }
 
-    if (aSurface == nullptr) {
-        LOG_ERROR("base64ToSurface");
+        return aSurface;
     }
-
-    return aSurface;
 }
