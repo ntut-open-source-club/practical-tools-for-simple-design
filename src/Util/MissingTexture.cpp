7d3bc583834ca762e62f9cb041c30a12bc477889
ntut-xuan <t109590031@ntut.org.tw>
2024-06-07 23:10:26 +0800

    Fix: Checker don't like my code :(

--- src/Util/MissingTexture.cpp
+++ src/Util/MissingTexture.cpp
@@ -5,29 +5,29 @@
 #include "Util/MissingTexture.hpp"
 
 namespace Util {
-    SDL_Surface *GetMissingImageTextureSDLSurface() {
-        SDL_RWops *rwop =
-                SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
-                                   MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
-        SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
+SDL_Surface *GetMissingImageTextureSDLSurface() {
+    SDL_RWops *rwop =
+            SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
+                               MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
+    SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
 
-        if (aSurface == nullptr) {
-            LOG_ERROR("base64ToSurface");
-        }
-
-        return aSurface;
+    if (aSurface == nullptr) {
+        LOG_ERROR("base64ToSurface");
     }
 
-    SDL_Surface *GetMissingFontTextureSDLSurface() {
-        SDL_RWops *rwop =
-                SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
-                                   MISSING_FONT_TEXTURE_BASE64_DECODE.size());
-        SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "JPG");
+    return aSurface;
+}
 
-        if (aSurface == nullptr) {
-            LOG_ERROR("base64ToSurface");
-        }
+SDL_Surface *GetMissingFontTextureSDLSurface() {
+    SDL_RWops *rwop =
+            SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
+                               MISSING_FONT_TEXTURE_BASE64_DECODE.size());
+    SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "JPG");
 
-        return aSurface;
+    if (aSurface == nullptr) {
+        LOG_ERROR("base64ToSurface");
     }
+
+    return aSurface;
+}
 }
