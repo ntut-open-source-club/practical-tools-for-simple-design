95b9f7e916590f30a97de939df386d24fc48f947
ntut-xuan <t109590031@ntut.org.tw>
2024-06-07 23:20:18 +0800

    I am physical hacker

--- src/Util/MissingTexture.cpp
+++ src/Util/MissingTexture.cpp
@@ -6,8 +6,9 @@
 
 namespace Util {
 SDL_Surface *GetMissingImageTextureSDLSurface() {
-    SDL_RWops *rwop = SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
-                                         MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
+    SDL_RWops *rwop =
+        SDL_RWFromConstMem(MISSING_IMAGE_TEXTURE_BASE64_DECODE.data(),
+                           MISSING_IMAGE_TEXTURE_BASE64_DECODE.size());
     SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "PNG");
 
     if (aSurface == nullptr) {
@@ -18,8 +19,9 @@ SDL_Surface *GetMissingImageTextureSDLSurface() {
 }
 
 SDL_Surface *GetMissingFontTextureSDLSurface() {
-    SDL_RWops *rwop = SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
-                                         MISSING_FONT_TEXTURE_BASE64_DECODE.size());
+    SDL_RWops *rwop =
+        SDL_RWFromConstMem(MISSING_FONT_TEXTURE_BASE64_DECODE.data(),
+                           MISSING_FONT_TEXTURE_BASE64_DECODE.size());
     SDL_Surface *aSurface = IMG_LoadTyped_RW(rwop, 1, "JPG");
 
     if (aSurface == nullptr) {
@@ -28,4 +30,5 @@ SDL_Surface *GetMissingFontTextureSDLSurface() {
 
     return aSurface;
 }
-}
+} // namespace Util
+
